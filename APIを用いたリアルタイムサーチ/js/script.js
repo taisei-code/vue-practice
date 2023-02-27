88b32b1a075d30766758abc6257474a2b1e01477
Taisei <taisei96.web@gmail.com>
2023-02-27 14:55:56 +0900

    API実装

--- APIを用いたリアルタイムサーチ/js/script.js
+++ APIを用いたリアルタイムサーチ/js/script.js
@@ -13,11 +13,35 @@ const app = Vue.createApp({
   },
 
   mounted: function() {
-  
+    this.keyword = 'JS'
+    this.getAnswer()
   },
 
   methods: {
-    
+    /* APIからデータを取得するメソット */
+    getAnswer: function() {
+      if(this.keyword === '') {
+        console.log('karamoji')
+        this.items = null
+        return
+      }
+
+      this.message = 'Loading'
+      const vm     = this
+      const params = { page: 1, per_page: 20, query: this.keyword }
+
+      /* axiosのgetメソットで使ってAPIを叩く */
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response) {
+          console.log(response)
+        })
+        .catch(function(error) {
+          vm.message = 'Error' + error
+        })
+        .finally(function() {
+          vm.message = ''
+        })
+    }
   }
 
 })
