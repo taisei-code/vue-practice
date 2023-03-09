70c70b880615a4da0dbaa951b70ee4b9699ddc67
Taisei <taisei96.web@gmail.com>
2023-03-09 14:23:09 +0900

    コンポーネント動的処理

--- 13- コンポーネントを使おう/ローカル/js/script.js
+++ 13- コンポーネントを使おう/ローカル/js/script.js
@@ -1,5 +1,13 @@
-const helloComponent = {
-  template: '<p>Hello</p>'
+const buttonCounter = {
+  template: '<div><span>count: </span><button v-on:click="countUp">{{ count }}</button></div>',
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    countUp: function(event) {
+      this.count++
+    }
+  }
 }
 
 const app = Vue.createApp({
@@ -7,7 +15,7 @@ const app = Vue.createApp({
     
   }),
   components: {
-    'hello-component': helloComponent
+    'button-counter': buttonCounter
   }
 })
 
